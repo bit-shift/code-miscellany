3dee4aee910921a87db7572dd9d32d59bb0272c4
Bit Shift <bitshift@bigmacintosh.net>
2013-12-14 04:46:17 +0000

    Adjust wrapnote to wrap any kind/size of indent.

--- wrapnote.nim
+++ wrapnote.nim
@@ -1,9 +1,17 @@
 import strutils
 
+proc indentedWrap(line: string, maxLineWidth = 80): seq[string] =
+    var strippedLine = line.strip(leading=true, trailing=false)
+
+    var indentSize = len(line) - len(strippedLine)
+    var wrapWidth = maxLineWidth - indentSize
+
+    var wrappedLines = wordWrap(strippedLine, maxLineWidth = wrapWidth)
+
+    var prefix = line.substr(0, indentSize - 1)
+    return splitLines(wrappedLines).map(proc (s: string): string =
+        prefix & s.strip(leading=true, trailing=false))
 
 for line in lines(stdin):
-    if startsWith(line, "   "):
-        for wrappedLine in splitLines(wordWrap(unindent(line, true), maxLineWidth = 77)):
-            writeln(stdout, "   " & wrappedLine)
-    else:
-        writeln(stdout, line)
+    for wrappedLine in indentedWrap(line):
+        writeln(stdout, wrappedLine)
