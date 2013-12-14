d119f4d1784b4d2314e58c2f3f8460e34c635c3d
Bit Shift <bitshift@bigmacintosh.net>
2013-12-14 02:37:14 +0000

    Add no-args mode where it just dumps the note file. (Aliased as @@='jotnote | less' here)

--- jotnote.sh
+++ jotnote.sh
@@ -3,10 +3,17 @@
 JOTNOTE_FILE="${JOTNOTE_FILE:-${HOME}/.jotnote}"
 touch "${JOTNOTE_FILE}"
 
-echo -n "$(date --iso-8601=seconds)   " >> "${JOTNOTE_FILE}"
-echo "$(pwd)" >> "${JOTNOTE_FILE}"
-echo "   $@" >> "${JOTNOTE_FILE}"
-echo >> "${JOTNOTE_FILE}"
+case "$1" in
+    "")
+        cat "${JOTNOTE_FILE}"
+        ;;
+    *)
+        echo -n "$(date --iso-8601=seconds)   " >> "${JOTNOTE_FILE}"
+        echo "$(pwd)" >> "${JOTNOTE_FILE}"
+        echo "   $@" >> "${JOTNOTE_FILE}"
+        echo >> "${JOTNOTE_FILE}"
 
-echo
-tail -n3 "${JOTNOTE_FILE}" | head -n2
+        echo
+        tail -n3 "${JOTNOTE_FILE}" | head -n2
+        ;;
+esac
